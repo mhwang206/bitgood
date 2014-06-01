a9c01072ec0ac6d1e9b628f608f39608f29e701e
Matt Hwang <mhwang206@gmail.com>
2014-06-01 13:47:01 -0700

    add why bitcoin and problem page

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,8 @@ Rails.application.routes.draw do
   root 'organizations#index'
   get "about" => "pages#about"
   get "howto" => "pages#howto"
+  get "problem" => "pages#problem"
+  get "why" => "pages#why"
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
