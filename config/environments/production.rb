e7a91763a8ce573fc8887e09c6dadcfc038b7944
Matt Hwang <mhwang206@gmail.com>
2014-05-31 22:18:08 -0700

    implement devise

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -80,4 +80,7 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  # Set this to your actual url
+  config.action_mailer.default_url_options = { :host => 'http://bitgood.herokuapp.com/' }
 end
